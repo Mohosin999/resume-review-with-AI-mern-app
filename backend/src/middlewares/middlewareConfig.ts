650c3e2792c36876674b4044f258792d2406e76e
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-01 21:43:55 +0600

    backend

--- backend/src/middlewares/middlewareConfig.ts
+++ backend/src/middlewares/middlewareConfig.ts
@@ -43,7 +43,7 @@ export const applyMiddleware = (app: Application): void => {
 
   const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 1000,
+    max: 10,
     message: { message: "Too many requests, please try again later." },
     standardHeaders: true,
     legacyHeaders: false,
