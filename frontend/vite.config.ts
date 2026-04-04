4569bb0a3baa02507d6707a8c5a2a64a40af2687
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-04 09:38:55 +0600

    refactor: Update vite.config.ts file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,16 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  },
+
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:5000",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+  // build: {
+  //   outDir: "build",
+  //   sourcemap: true,
+  // },
 });
